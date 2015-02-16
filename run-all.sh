a9737baac3baaa738834e21d2c482e907d644493
Matteo Abis <gmatteo..abis@gmail.com>
2015-02-16 11:16:00 +0100

    add script

---
+++ run-all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd ../analysis && forever -w start app.js
+cd ../analysis-frontend && middleman server &
+cd ../rzmq-workers && ./rzmq-worker.R 
+cd ../analysis && forever logs app.js -f
