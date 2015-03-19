821f1b16e24ce3929bae7a9f313daad332c9c7df
luvemil <taran.marco@gmail.com>
2015-03-19 18:51:05 +0100

    Ensure sails stops

--- run-all.sh
+++ run-all.sh
@@ -4,3 +4,5 @@ cd ../analysis && forever -w start app.js
 cd ../analysis-frontend && middleman server &
 cd ../rzmq-workers && ./rzmq-worker.R &
 cd ../analysis && forever logs app.js -f
+
+forever stop app.js
